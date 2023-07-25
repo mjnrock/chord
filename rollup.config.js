c7f755e3eef019fada497cf49bafdb207cf74b50
LesPantsFancy <mjnrock@umich.edu>
2023-07-25 19:38:37 -0400

    init

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ export default {
 		file: "dist/main.js",
 		format: "esm", // Use "esm" to output ES modules that are compatible with browser and latest Node.js versions
 	},
+    external: [ "react", "react-dom" ], // add this line
 	plugins: [
 		babel({
 			exclude: "node_modules/**",
