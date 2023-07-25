d950763d15963ad25b38fd68d1c92271a9b859b3
LesPantsFancy <mjnrock@umich.edu>
2023-07-25 19:11:53 -0400

    init

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,34 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
 
 export default {
-	input: './src/main.js',
-	output: {
-		file: './dist/main.js',
-		format: 'esm',
-	},
+	input: 'src/main.js',
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs',
+			exports: 'named',
+			sourcemap: true,
+		},
+		{
+			file: pkg.module,
+			format: 'es',
+			exports: 'named',
+			sourcemap: true,
+		},
+	],
 	plugins: [
-		resolve(),
-		commonjs(),
 		babel({
 			exclude: 'node_modules/**',
-			presets: [ '@babel/preset-env', [ '@babel/preset-react', { "runtime": "automatic" } ] ],
+			presets: [
+				'@babel/preset-env',
+				[ '@babel/preset-react', { runtime: 'automatic' } ],
+			],
 		}),
+		resolve(),
+		commonjs(),
 	],
-	external: [ 'react', 'uuid' ],
+	external: [ 'react', 'react-dom' ],
 };
