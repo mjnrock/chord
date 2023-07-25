1007b97fefc481de1408c5b24fd092b51e8c0aa8
LesPantsFancy <mjnrock@umich.edu>
2023-07-25 17:33:59 -0400

    init

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
-import babel from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-	input: "src/main.js",
+	input: './src/main.js',
 	output: {
-		file: "dist/main.js",
-		format: "esm",
+		file: 'dist/main.js',
+		format: 'esm'
 	},
+	external: [ 'react' ], // <-- Here is where you define 'react' as an external dependency
 	plugins: [
 		resolve(),
-		commonjs(),  // Add this line
-		babel({
-			babelHelpers: "bundled",
-			presets: [ "@babel/preset-env", "@babel/preset-react" ]
-		}),
+		commonjs(),
+		babel({ babelHelpers: 'bundled' }),
+		terser()
 	],
 };
