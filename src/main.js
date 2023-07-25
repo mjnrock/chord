7a27ef895fd8ad09c6960a89820c1a56a7c884f8
LesPantsFancy <mjnrock@umich.edu>
2023-07-25 16:50:51 -0400

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import Form from "./@form/package.js";
 import Node from "./@node/package.js";
 
-export default {
-	Form,
-	Node,
-};
+export { Form, Node };
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,23 +2,29 @@ const path = require("path");
 
 module.exports = {
 	entry: "./src/main.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "main.js",
+		library: "@lespantsfancy/chord",
+		libraryTarget: "umd",
+		globalObject: "this",
+		umdNamedDefine: true,
+	},
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
+				exclude: /(node_modules|bower_components)/,
 				use: {
-					loader: "babel-loader"
+					loader: "babel-loader",
+					options: {
+						presets: [ "@babel/preset-env", "@babel/preset-react" ]
+					}
 				}
 			}
 		]
 	},
 	resolve: {
-		extensions: [ "*", ".js", ".jsx" ]
-	},
-	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "dist"),
-		libraryTarget: "umd"
+		extensions: [ ".jsx", ".js" ],
 	},
 };
