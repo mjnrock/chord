5fc5cb8acfe4c484add37249d994c0b48fe6da3f
LesPantsFancy <mjnrock@umich.edu>
2023-07-29 10:11:05 -0400

    init

--- src/@node/Registry.js
+++ src/@node/Registry.js
@@ -1,5 +1,5 @@
 import { v4 as uuid, validate } from "uuid";
-import Identity from "./Identity";
+import Identity from "./Identity.js";
 
 /*
  * @Schema = {
@@ -19,21 +19,37 @@ import Identity from "./Identity";
 * };
 */
 
+const isNamespace = (registry, key) => {
+	const entry = registry[ key ];
+
+	if(entry.type === EnumEntryType.ALIAS) {
+		return isNamespace(registry, entry.value);
+	}
+
+	return entry && entry.type === EnumEntryType.NAMESPACE;
+};
+
+const findNamespace = (registry, namespace) => {
+	let namespaceEntry = registry[ namespace ];
+	return isNamespace(namespaceEntry) ? namespaceEntry.value : null;
+};
+
 export const EnumEntryType = {
 	ENTRY: 0,
 	ALIAS: 1,
 	POOL: 2,
+	NAMESPACE: 3,
 };
 
-export const RegistryEntry ={
-	Next({ type, value, ...registry } = {}) {
+export const RegistryEntry = {
+	Next({ type, value, id, ...entry } = {}) {
 		return {
-			...registry,
+			...entry,
 			type,
 			value,
 		};
 	},
-	New({ type, value, ...rest } = {}) {
+	New({ type, value, id, ...rest } = {}) {
 		return RegistryEntry.Next({
 			type,
 			value,
@@ -153,6 +169,34 @@ export const Registry = {
 
 		return true;
 	},
+
+	setNamespace(registry, alias, ...args) {
+		const entry = Registry.New(...args);
+
+		if(alias.startsWith("$")) {
+			let keys = alias.split(".");
+			alias = keys.pop();
+
+			let parentNamespace = Resolver.getValueByPath(registry, keys.join("."));
+			if(parentNamespace?.$id in registry) {
+				registry = parentNamespace;
+			} else {
+				return false;
+			}
+		}
+
+		registry[ entry.$id ] = RegistryEntry.New({
+			type: EnumEntryType.NAMESPACE,
+			value: entry,
+		});
+
+		Registry.addAlias(registry, entry.$id, alias);
+
+		return true;
+	},
+	removeNamespace(registry, name) {
+		delete registry[ name ];
+	},
 };
 
 export const Reader = {
@@ -178,7 +222,7 @@ export const Reader = {
 		if(!(name in registry)) {
 			return;
 		}
-		
+
 		if(resolve) {
 			return registry[ name ].value.map(id => registry[ id ].value);
 		} else {
@@ -208,8 +252,182 @@ export const Writer = {
 	},
 };
 
+export const Resolver = {
+	getValueByPath(registry, path) {
+		// Split the path into keys
+		let keys = path.split(".");
+
+		// Start at the root registry ($)
+		let rootKey = keys.shift();
+		if(rootKey !== "$") {
+			throw new Error("Path must start at the root registry ($)");
+		}
+
+		// Recursive function for path resolution
+		function resolvePath(currentRegistry, remainingKeys) {
+			// If there are no keys remaining, we've reached our target.
+			if(remainingKeys.length === 0) {
+				return currentRegistry;
+			}
+
+			let currentKey = remainingKeys.shift();
+
+			// If the current registry does not contain the current key, then the path does not exist.
+			if(!(currentKey in currentRegistry)) {
+				return;
+			}
+
+			// Get the next item in the registry
+			let nextItem = currentRegistry[ currentKey ];
+
+			// If the next item is a namespace, update the current registry and get the next key.
+			if(nextItem.type === EnumEntryType.NAMESPACE) {
+				return resolvePath(nextItem.value, remainingKeys);
+			} else if(nextItem.type === EnumEntryType.ALIAS) {
+				// If the current key is an alias, get the actual UUID and find that in the registry
+				let uuid = nextItem.value;
+				if(uuid in currentRegistry) {
+					nextItem = currentRegistry[ uuid ];
+					if(nextItem.type === EnumEntryType.NAMESPACE) {
+						// If the alias pointed to a namespace, continue resolving the path in the namespace
+						return resolvePath(nextItem.value, remainingKeys);
+					} else {
+						// If the alias pointed to something other than a namespace, return its value
+						return nextItem.value;
+					}
+				} else {
+					// If the alias' UUID isn't found in the registry, return undefined
+					return;
+				}
+			} else {
+				// If the current key points to a value, return its value.
+				return nextItem.value;
+			}
+		}
+
+		// Start the recursive path resolution
+		return resolvePath(registry, keys);
+	},
+	getIdByPath(registry, path) {
+		// Split the path into keys
+		let keys = path.split(".");
+
+		// Start at the root registry ($)
+		let rootKey = keys.shift();
+		if(rootKey !== "$") {
+			throw new Error("Path must start at the root registry ($)");
+		}
+
+		// Recursive function for path resolution
+		function resolvePath(currentRegistry, remainingKeys) {
+			// If there are no keys remaining, we've reached our target.
+			if(remainingKeys.length === 0) {
+				// Instead of returning the value, return the ID of the final key
+				return currentRegistry.$id;
+			}
+
+			let currentKey = remainingKeys.shift();
+
+			// If the current registry does not contain the current key, then the path does not exist.
+			if(!(currentKey in currentRegistry)) {
+				return;
+			}
+
+			// Get the next item in the registry
+			let nextItem = currentRegistry[ currentKey ];
+
+			// If the next item is a namespace, update the current registry and get the next key.
+			if(nextItem.type === EnumEntryType.NAMESPACE) {
+				return resolvePath(nextItem.value, remainingKeys);
+			} else if(nextItem.type === EnumEntryType.ALIAS) {
+				// If the current key is an alias, get the actual UUID and find that in the registry
+				let uuid = nextItem.value;
+				if(uuid in currentRegistry) {
+					nextItem = currentRegistry[ uuid ];
+					if(nextItem.type === EnumEntryType.NAMESPACE) {
+						// If the alias pointed to a namespace, continue resolving the path in the namespace
+						return resolvePath(nextItem.value, remainingKeys);
+					} else {
+						// If the alias pointed to something other than a namespace, return the ID.
+						return uuid;
+					}
+				} else {
+					// If the alias' UUID isn't found in the registry, return undefined
+					return;
+				}
+			} else {
+				// If the current key points to a value, return its ID.
+				return nextItem.$id;
+			}
+		}
+
+		// Start the recursive path resolution
+		return resolvePath(registry, keys);
+	},
+};
+
+export const Transformer = {
+	flatten(registry, parentId = null, result = []) {
+		if(!parentId) {
+			parentId = registry.$id;
+		}
+
+		Object.entries(registry).forEach(([ id, entry ]) => {
+			if(id[ 0 ] === "$") {
+				return; // Skip meta data, as we're just flattening records
+			}
+
+			const { type, value } = entry;
+
+			switch(type) {
+				case EnumEntryType.ENTRY:
+					result.push({
+						ID: id,
+						PID: parentId,
+						Type: EnumEntryType.ENTRY,
+						Value: value,
+					});
+					break;
+				case EnumEntryType.ALIAS:
+					result.push({
+						ID: null, // Aliases have a null ID
+						PID: value, // ParentId for an alias is the Id of the entry it points to
+						Type: EnumEntryType.ALIAS,
+						Value: id,
+					});
+					break;
+				case EnumEntryType.POOL:
+					result.push({
+						ID: id, // Id for a pool is its key
+						PID: parentId, // ParentId for a pool is the immediate parent registry to it
+						Type: EnumEntryType.POOL,
+						Value: value, // Value for a pool is its normal value
+					});
+					break;
+				case EnumEntryType.NAMESPACE:
+					result.push({
+						ID: id,
+						PID: parentId,
+						Type: EnumEntryType.NAMESPACE,
+						Value: value, // Value for a namespace is its normal value
+					});
+					Transformer.flatten(value, id, result); // Recurse into namespace
+					break;
+				default:
+					throw new Error("Unknown entry type: " + type);
+			}
+		});
+
+		return result;
+	}
+};
+
+
 export default {
 	Registry,
 	Reader,
 	Writer,
+
+	Resolver,
+	Transformer,
 };
---
+++ tests/Registry/general.test.js
@@ -0,0 +1,36 @@
+import { EnumEntryType, Reader, Registry, Resolver, Transformer } from "../../src/@node/Registry.js";
+
+// Simple registry for testing
+const registry = Registry.New({
+	"rootEntry": "Hello World",
+});
+
+Registry.addPool(registry, "rootPool");
+Registry.addToPool(registry, "rootPool", Resolver.getIdByPath(registry, "$.rootEntry"));
+
+Registry.setNamespace(registry, "ns1", {
+	"ns1Entry": "Hello from ns1",
+});
+
+Registry.setNamespace(registry, "$.ns1.ns2", {
+	"ns2Entry": "Hello from ns2",
+});
+
+console.log(JSON.stringify(registry, null, 2));
+
+// Testing Resolver
+// console.log("0:", Resolver.getValueByPath(registry, "$"));
+// console.log("1:", Resolver.getValueByPath(registry, "$.rootEntry"));
+// console.log("1a:", Resolver.getIdByPath(registry, "$.rootEntry"));
+// console.log("1b:", Resolver.getIdByPath(registry, "$.ns1.ns2"));
+
+// console.log("2:", Resolver.getByPath(registry, "$.ns1"));
+// console.log("3:", Resolver.getByPath(registry, "$.ns1.ns1Entry"));
+// console.log("4:", Resolver.getByPath(registry, "$.ns1.ns2"));
+// console.log("5:", Resolver.getByPath(registry, "$.ns1.ns2.ns2Entry"));
+
+// console.log("------------------------");
+
+// // Testing Transformer
+const flattened = Transformer.flatten(registry);
+console.table(flattened);
